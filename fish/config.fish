6a6ad2fa5cba73b5d20a9c7c5e0b5f9f478ba0ac
Ryan Resella <ryanatwork@users.noreply.github.com>
2016-09-01 12:40:05 -0700

    Add secret config file

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 set -g fish_greeting ''
 
+set -x SECFILES $HOME/.secret
+
 set -x EDITOR (which vim)
 set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH /usr/local/bin $PATH
@@ -12,4 +14,7 @@ set -x RUBYGEMS_EC2_DB1 ec2-54-245-133-190.us-west-2.compute.amazonaws.com
 set -x RUBYGEMS_EC2_LB1 54.245.255.174
 set -x JAVA_HOME (/usr/libexec/java_home)
 set -x NVM_DIR /Users/ryanresella/.nvm
+
+# Import secret config
+[ -f $SECFILES/fish/config.fish ]; and . $SECFILES/fish/config.fish
 
